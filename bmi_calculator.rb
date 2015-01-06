884442d6fc9afa6995d2c51b998bb850d62d6b71
William Holt <wtholt27@gmail.com>
2015-01-06 14:50:03 -0500

    created bmi calculator

--- bmi_calculator.rb
+++ bmi_calculator.rb
@@ -6,9 +6,19 @@ if answer == "yes"
 	puts "Please enter your height in inches: "
 	height = gets.chomp
 	puts "Thanks for your input! Please wait...system processing"
+	sleep(3)
 	bmi = 703.0 * weight.to_f/height.to_f ** 2.0
 	puts "Your BMI is #{bmi.to_f}"
 
+		if bmi < 18.5
+			puts "You are underweight."
+		elsif bmi <= 24.9
+			puts "Congratulations, you are at a normal weight."
+		elsif bmi >= 25.0
+			puts "You are overweight."
+		elsif bmi >= 30.0
+			puts "Uh oh! You are obese."
+		end
 else
 	puts "OK you don't have to measure your BMI"
 end
